2b1f6705bea2fa8a09cfc6b5b6bcf82e50135a6e
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-10 19:44:07 -0300

    Added Funds Model - Problem with presence true

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ NewApp::Application.routes.draw do
   get "home/index"
 
   resources :posts
+  resources :funds
 
   root :to => 'Home#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508023033) do
+ActiveRecord::Schema.define(:version => 20120510223413) do
+
+  create_table "funds", :force => true do |t|
+    t.string   "name"
+    t.integer  "minapp"
+    t.string   "category"
+    t.text     "trackrecord"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
